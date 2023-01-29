30f63a4cbf16f36fc7efb918bfee644192edf845
Petar Stoqnov <desireon98@gmail.com>
2023-01-29 20:47:50 +0200

    Create new interface BinProviderInterface.php.

---
+++ src/External/BinProviderInterface.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\External;
+
+use App\Exception\BinlistException;
+use App\Model\Transaction;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\DecodingExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
+
+interface BinProviderInterface
+{
+    public const BINLIST = 'binlist';
+
+    /**
+     * @param Transaction $transaction
+     * @return string
+     * @throws BinlistException
+     * @throws ClientExceptionInterface
+     * @throws DecodingExceptionInterface
+     * @throws RedirectionExceptionInterface
+     * @throws ServerExceptionInterface
+     * @throws TransportExceptionInterface
+     */
+    public function getCountry(Transaction $transaction): string;
+}
